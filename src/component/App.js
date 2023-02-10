cd834b9add96c11e84611ff20c179c2cea800f58
Yalalt <yalaltgandush@gmail.com>
2023-02-10 10:55:46 +0800

    Project file screated initialize

---
+++ src/component/App.js
@@ -0,0 +1,12 @@
+import '../style/App.css';
+import Login from './Login';
+
+function App() {
+  return (
+    <div className="app-container">
+      <Login />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Layout.js
@@ -0,0 +1,21 @@
+const Layout = (props) => {
+  {
+    /* 
+        App 
+        Layout
+            Header
+                Menu
+            Home
+                Basket
+                    Orders
+                Logout
+                Products List
+                    Product
+            Login
+            Register            
+            Footer */
+  }
+
+  return <div>{props.children}</div>;
+};
+export default Layout;
